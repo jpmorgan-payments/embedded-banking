64d10e7f4b1aa6c7031c29bc2ccffe37534a373c
Hyungho Seo <hyunghoseo@gmail.com>
2024-02-12 16:08:48 -0500

    Update eslint config

--- embedded-components/.eslintrc.cjs
+++ embedded-components/.eslintrc.cjs
@@ -2,8 +2,10 @@ module.exports = {
   extends: ['mantine'],
   parserOptions: {
     project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
+  ignorePatterns: ['*.js', '*.cjs', '*.mjs', '*.d.ts', '*.d.mts', 'vite.config.ts'],
 };
