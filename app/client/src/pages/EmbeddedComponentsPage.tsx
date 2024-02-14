e424d24e151aa1b6d7fbb5cdfd1ddbfe9a578dcb
Hyungho Seo <hyunghoseo@gmail.com>
2024-02-14 16:21:32 -0500

    Add updated eb components package and fix build

--- app/client/src/pages/EmbeddedComponentsPage.tsx
+++ app/client/src/pages/EmbeddedComponentsPage.tsx
@@ -2,13 +2,18 @@ import { Box, Card, Text } from '@mantine/core';
 
 import { PageWrapper } from 'components';
 import { GITHUB_REPO } from 'data/constants';
-import { LinkAccountForm } from 'embedded-banking-components';
+import {
+  LinkAccountForm,
+  EBComponentsProvider,
+} from 'embedded-banking-components';
 
 export const EmbeddedComponentsPage = () => {
   return (
     <PageWrapper title="Embedded Components">
       <Card maw={300} shadow="lg" radius={0} withBorder>
-        <LinkAccountForm />
+        <EBComponentsProvider theme={{}}>
+          <LinkAccountForm />
+        </EBComponentsProvider>
       </Card>
     </PageWrapper>
   );
