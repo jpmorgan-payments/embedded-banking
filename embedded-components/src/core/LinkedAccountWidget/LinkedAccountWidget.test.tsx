220684aafb2579f6b664f2968b1c07424becd3aa
Hyungho Seo <hyunghoseo@gmail.com>
2024-03-29 15:16:14 -0400

    add simple test for LinkedAccountWidget

---
+++ embedded-components/src/core/LinkedAccountWidget/LinkedAccountWidget.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@test-utils';
+
+import { LinkedAccountWidget } from './LinkedAccountWidget';
+
+describe('LinkedAccountWidget component', () => {
+  it('renders without error', () => {
+    render(<LinkedAccountWidget />);
+  });
+});
