25aa06f2d2649f4c4304581dc43de1aa1a123297
Hyungho Seo <hyungho.seo@jpmchase.com>
2024-04-03 11:05:18 -0400

    update layout of link account form

--- embedded-components/src/core/LinkedAccountWidget/LinkedAccountWidget.tsx
+++ embedded-components/src/core/LinkedAccountWidget/LinkedAccountWidget.tsx
@@ -1,28 +1,17 @@
-import { useState } from 'react';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
 
-import { LinkAccountForm } from './LinkAccountForm/LinkAccountForm';
+import { LinkAccountFormDialogTrigger } from './LinkAccountForm/LinkAccountForm';
 import { LinkAccountFormSchema } from './LinkAccountForm/LinkAccountForm.schema';
 
 export const LinkedAccountWidget = () => {
   const accountStatus = 'notLinked';
-  const [isDialogOpen, setDialogOpen] = useState(false);
 
   const handleSubmit = (data: z.infer<typeof LinkAccountFormSchema>) => {
     console.log(data);
     // Handle account linking logic here
-    setDialogOpen(false);
   };
 
   return (
@@ -33,23 +22,9 @@ export const LinkedAccountWidget = () => {
       <CardContent>
         <div className="eb-flex eb-items-center eb-space-x-4 eb-rounded-md eb-border eb-p-4">
           {accountStatus === 'notLinked' && (
-            <Dialog
-              open={isDialogOpen}
-              onOpenChange={(open) => {
-                setDialogOpen(open);
-              }}
-            >
-              <DialogTrigger asChild>
-                <Button>Link Account</Button>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Link an account</DialogTitle>
-                  <DialogDescription>This is a description</DialogDescription>
-                </DialogHeader>
-                <LinkAccountForm onSubmit={handleSubmit} />
-              </DialogContent>
-            </Dialog>
+            <LinkAccountFormDialogTrigger onSubmit={handleSubmit}>
+              <Button>Link Account</Button>
+            </LinkAccountFormDialogTrigger>
           )}
         </div>
       </CardContent>
