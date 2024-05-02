a17eafe361e68010da2db233ca87683898ea2fdb
Marie Halloran <marie.halloran@jpmchase.com>
2024-05-02 16:18:47 -0400

    make questions look nicer

--- embedded-components/src/core/OnboardingWizard/Steps/QuestionsStep/QuestionsStep.tsx
+++ embedded-components/src/core/OnboardingWizard/Steps/QuestionsStep/QuestionsStep.tsx
@@ -3,10 +3,21 @@ import { useForm } from 'react-hook-form';
 
 import { smbdoListQuestions } from '@/api/generated/embedded-banking';
 import { SchemasQuestionResponse } from '@/api/generated/embedded-banking.schemas';
-import { Form } from '@/components/ui/form';
-import { Separator } from '@/components/ui/separator';
-import { Text } from '@/components/ui/text';
-import { Title } from '@/components/ui/title';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Grid,
+  RadioGroup,
+  RadioGroupItem,
+  Separator,
+  Stack,
+  TextArea,
+  Title,
+} from '@/components/ui';
 
 import { useOnboardingForm } from '../../context/form.context';
 import NavigationButtons from '../../Stepper/NavigationButtons';
@@ -14,6 +25,75 @@ import NavigationButtons from '../../Stepper/NavigationButtons';
 type QuestionsStepProps = {
   setActiveStep: any;
   activeStep: number;
+};
+
+type QuestionProps = {
+  question: SchemasQuestionResponse;
+  form: any;
+};
+
+const Question = ({ question, form }: QuestionProps) => {
+  return (
+    <>
+      {question?.responseSchema?.items?.type === 'boolean' ? (
+        <FormField
+          control={form.control}
+          name={`${question?.id}`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="eb-my-5" asterisk>
+                {question?.content && question.content[0].label}
+              </FormLabel>
+
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="eb-flex eb-flex-row eb-space-y-1"
+                >
+                  <FormItem className="eb-flex eb-items-center eb-space-x-3 eb-space-y-0">
+                    <RadioGroupItem value="yes" />
+
+                    <FormLabel className="eb-font-normal">Yes</FormLabel>
+                  </FormItem>
+                  <FormItem className="eb-flex eb-items-center eb-space-x-3 eb-space-y-0">
+                    <RadioGroupItem value="no" />
+
+                    <FormLabel className="eb-font-normal">No</FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      ) : (
+        <FormField
+          control={form.control}
+          name={`${question?.id}`}
+          render={({ field }) => (
+            <FormItem>
+              <Stack>
+                <FormLabel className="eb-my-5" asterisk>
+                  {question?.content && question.content[0].label}
+                </FormLabel>
+                <FormControl>
+                  <TextArea
+                    {...field}
+                    size="md"
+                    className="eb-h-30 eb-border-solid eb-border"
+                  />
+                </FormControl>
+                <FormMessage />
+              </Stack>
+            </FormItem>
+          )}
+        />
+      )}
+      <Separator />
+    </>
+  );
 };
 
 const QuestionsStep = ({ setActiveStep, activeStep }: QuestionsStepProps) => {
@@ -58,12 +138,11 @@ const QuestionsStep = ({ setActiveStep, activeStep }: QuestionsStepProps) => {
         </Title>
 
         <Separator />
-        {questions?.map((question) => (
-          <>
-            <Text key={question.id}>{question.content[0].label}</Text>
-            <Separator />
-          </>
-        ))}
+        <Grid className={`eb-gap-4 eb-pt-4 ${'eb-grid-flow-row'} `}>
+          {questions?.map((question) => (
+            <Question questions={questions} question={question} form={form} />
+          ))}
+        </Grid>
         <NavigationButtons
           onSubmit={onSubmit}
           setActiveStep={setActiveStep}
