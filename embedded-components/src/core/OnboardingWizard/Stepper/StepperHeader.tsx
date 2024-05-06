fd1464d2e875ed563661d088b9c5675335f08ca1
Marie Halloran <marie.halloran@jpmchase.com>
2024-05-06 14:25:32 -0400

    improve stepper style

--- embedded-components/src/core/OnboardingWizard/Stepper/StepperHeader.tsx
+++ embedded-components/src/core/OnboardingWizard/Stepper/StepperHeader.tsx
@@ -9,38 +9,36 @@ import {
 
 import { Text } from '@/components/ui/text';
 
-import { useStepper } from './Stepper';
-
 const icons = [
   {
     title: 'Personal',
-    icon: <User key="userIcon" size={40} color="black"></User>,
+    icon: <User key="userIcon" size={20} color="black"></User>,
   },
   {
     title: 'Business',
-    icon: <Building key="buildingIcon" size={40} color="black"></Building>,
+    icon: <Building key="buildingIcon" size={20} color="black"></Building>,
   },
   {
     title: 'Decision Makers',
-    icon: <Users key="userIcon" size={40} color="black"></Users>,
+    icon: <Users key="userIcon" size={20} color="black"></Users>,
   },
   {
     title: 'Additional Info',
-    icon: <FileInfo key="fileIcon" size={40} color="black"></FileInfo>,
+    icon: <FileInfo key="fileIcon" size={20} color="black"></FileInfo>,
   },
   {
     title: 'Review',
     icon: (
       <LayoutDashboard
         key="layoutIcon"
-        size={40}
+        size={20}
         color="black"
       ></LayoutDashboard>
     ),
   },
   {
     title: 'Verifications',
-    icon: <Checklist key="checklistIcon" size={40} color="black"></Checklist>,
+    icon: <Checklist key="checklistIcon" size={20} color="black"></Checklist>,
   },
 ];
 
@@ -51,28 +49,28 @@ type StepperHeaderProps = {
 
 const StepperHeader = ({ activeStep, setCurrentStep }: StepperHeaderProps) => {
   return (
-    <div className="eb-grid eb-grid-cols-6 eb-my-5 eb-cursor-pointer">
+    <div className="eb-flex eb-flex-row eb-justify-center sm:eb-hidden md:eb-flex eb-my-5 eb-cursor-pointer">
       {icons?.map((val, idx) => (
         <div
-          className="eb-grid eb-grid-cols-3 eb-w-26"
+          className="eb-flex eb-items-center eb-flex-row eb-gap-2"
           key={val.title}
           onClick={() => {
             setCurrentStep(idx + 1);
           }}
         >
           <div
-            className={`eb-flex eb-w-14 eb-h-14 eb-place-content-center eb-rounded-full eb-border-2 ${activeStep === idx + 1 ? 'eb-border-blue-800' : 'eb-border-primary/80'} eb-justify-items-center eb-justify-center eb-place-content-center eb-items-center`}
+            className={`eb-flex eb-w-12 eb-h-12 eb-place-content-center hover:eb-drop-shadow-sm eb-justify-center eb-rounded-full eb-border-2 ${activeStep === idx + 1 ? 'eb-border-primary/90' : 'eb-border-grey/80'} eb-justify-items-center eb-justify-center eb-place-content-center eb-items-center`}
           >
             {val?.icon}
           </div>
 
-          <div className="eb-flex eb-justify-center eb-place-content-center eb-items-center eb-mr-5">
-            <Text>{val?.title}</Text>
-          </div>
+        
+            <Text className="eb-text-sm eb-mr-2">{val?.title}</Text>
+      
           {idx !== icons?.length - 1 && (
-            <div className="eb-grid eb-grid-row-2 eb-w-12">
+            <div className="eb-grid eb-grid-row-2 sm:eb-hidden eb-mr-2 lg:eb-grid">
               <div></div>
-              <div className="eb-border-t-4"></div>
+              <div className="eb-border-t-2 eb-w-5"></div>
             </div>
           )}
         </div>
