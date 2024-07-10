4a4ddbd74a5bd045d4619d30c004190a77e81676
Hyungho Seo <hyungho.seo@jpmchase.com>
2024-07-10 16:35:55 -0400

    Handle various statuses of linked accounts

--- embedded-components/src/core/LinkedAccountWidget/LinkedAccountWidget.tsx
+++ embedded-components/src/core/LinkedAccountWidget/LinkedAccountWidget.tsx
@@ -1,9 +1,32 @@
+import { LinkIcon, PencilLineIcon } from 'lucide-react';
+
 import { useGetAllRecipients } from '@/api/generated/embedded-banking';
+import { RecipientStatus } from '@/api/generated/embedded-banking.schemas';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge, Separator } from '@/components/ui';
 
 import { LinkAccountFormDialogTrigger } from './LinkAccountForm/LinkAccountForm';
+
+const StatusBadge = ({ status }: { status: RecipientStatus }) => {
+  const propsMap: Record<RecipientStatus, Record<string, string>> = {
+    ACTIVE: {
+      variant: 'success',
+    },
+    MICRODEPOSITS_INITIATED: {
+      variant: 'secondary',
+    },
+    REJECTED: {
+      variant: 'destructive',
+    },
+    READY_FOR_VALIDATION: {},
+    INACTIVE: {
+      variant: 'secondary',
+    },
+  };
+
+  return <Badge {...propsMap[status]}>{status.replace('_', ' ')}</Badge>;
+};
 
 export const LinkedAccountWidget = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -40,17 +63,29 @@ export const LinkedAccountWidget = () => {
                         : recipient.partyDetails.businessName}
                       {` (...${recipient.account.number.slice(-4)})`}
                     </h4>
-                    <Badge>{recipient.status}</Badge>
+                    {recipient.status && (
+                      <StatusBadge status={recipient.status} />
+                    )}
                   </div>
                   <p className="eb-text-sm eb-text-muted-foreground">
                     {recipient.partyDetails.type.toLocaleUpperCase()}
                   </p>
+                  {recipient.status === 'MICRODEPOSITS_INITIATED' && (
+                    <div>
+                      <Button size="sm" variant="secondary" className="eb-mt-2">
+                        <PencilLineIcon className="eb-mr-2 eb-h-4 eb-w-4" />{' '}
+                        Verify microdeposits
+                      </Button>
+                    </div>
+                  )}
                 </div>
                 <Separator className="eb-my-4" />
               </div>
             ))}
           <LinkAccountFormDialogTrigger>
-            <Button>Link A New Account</Button>
+            <Button>
+              <LinkIcon className="eb-mr-2 eb-h-4 eb-w-4" /> Link A New Account
+            </Button>
           </LinkAccountFormDialogTrigger>
         </div>
       </CardContent>
