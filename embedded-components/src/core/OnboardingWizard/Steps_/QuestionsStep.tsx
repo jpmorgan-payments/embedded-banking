57f0bc8cc7b6b71346e30f0e377e7221393c09e3
Andrey Aleshintsev <andrey.aleshintsev@jpmchase.com>
2024-07-17 16:31:27 -0400

    just mainor update and pacakge

--- embedded-components/src/core/OnboardingWizard/Steps_/QuestionsStep.tsx
+++ embedded-components/src/core/OnboardingWizard/Steps_/QuestionsStep.tsx
@@ -11,6 +11,7 @@ import {
   SchemasQuestionResponse,
 } from '@/api/generated/embedded-banking.schemas';
 import { Grid, Stack, Title } from '@/components/ui';
+import { useRootConfig } from '@/core/EBComponentsProvider/RootConfigProvider';
 
 import { useOnboardingForm } from '../context/form.context';
 import { useFormSchema } from '../context/formProvider.contex';
@@ -20,7 +21,6 @@ import { updateOutstandingItems } from '../utils/actions';
 import { makeQuestionsAPIBody } from '../utils/apiUtilsParsers';
 import { useContentData } from '../utils/useContentData';
 import { q } from './q';
-import { useRootConfig } from '@/core/EBComponentsProvider/RootConfigProvider';
 
 const QuestionsStep = ({ questionsIds, children }: any) => {
   const { activeStep, setCurrentStep } = useStepper();
@@ -126,25 +126,14 @@ const QuestionsStep = ({ questionsIds, children }: any) => {
 
   const onSubmit = useCallback(async () => {
     const errors = form?.formState?.errors;
-    console.log('@@ON SUBMIT', errors, form.getValues());
     const questionList =
       questionsIds?.join(',') ||
       onboardingForm?.outstandingItems?.questionIds?.join(',');
 
     const postBody = makeQuestionsAPIBody(form.getValues(), questionList);
-    console.log(
-      '@@postBody',
-      form.getValues(),
-      '>>',
-      postBody,
-      questionList,
-      form.getValues(),
-      '>>',
-      questionsList
-    );
 
     const res = await submitQuestions({
-      id: onboardingForm?.id || clientId,
+      id: (onboardingForm?.id || clientId) as string,
       data: postBody,
     });
 
--- embedded-components/src/index.tsx
+++ embedded-components/src/index.tsx
@@ -4,6 +4,7 @@ export { LinkedAccountWidget } from './core/LinkedAccountWidget/LinkedAccountWid
 export { OnboardingWizard } from './core/OnboardingWizard/OnboardingWizard';
 export { OnboardingWizardRoot } from './core/OnboardingWizard/OnboardingWizardRoot';
 export { OnboardingWizardInit } from './core/OnboardingWizard/OboardingWizardInit';
+
 export * from './core/EBComponentsProvider';
 export * from './components/ui';
 export { StepperProvider } from './core/OnboardingWizard/Stepper/Stepper';
