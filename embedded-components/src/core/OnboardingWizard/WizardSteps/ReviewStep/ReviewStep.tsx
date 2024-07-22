d964eca770dcab91a6ad08837ad1157c22318907
Hyungho Seo <hyungho.seo@jpmchase.com>
2024-07-22 18:10:58 -0400

    add label-value fields in onboarding review step

--- embedded-components/src/core/OnboardingWizard/WizardSteps/ReviewStep/ReviewStep.tsx
+++ embedded-components/src/core/OnboardingWizard/WizardSteps/ReviewStep/ReviewStep.tsx
@@ -3,6 +3,10 @@ import { useFormContext } from 'react-hook-form';
 
 import { useSmbdoGetClient } from '@/api/generated/embedded-banking';
 import {
+  IndividualDetails,
+  OrganizationDetails,
+} from '@/api/generated/embedded-banking.schemas';
+import {
   Accordion,
   AccordionContent,
   AccordionItem,
@@ -29,6 +33,23 @@ import { useStepper } from '../../Stepper/useStepper';
 import { fromApiToForm } from '../../utils/fromApiToForm';
 // import { useContentData } from '../../utils/useContentData';
 import { reviewSchema } from '../StepsSchema';
+
+const LabelValueTable = ({
+  data,
+}: {
+  data: Array<{ label: string; value?: string }>;
+}) => {
+  return (
+    <dl className="eb-grid eb-gap-3">
+      {data.map(({ label, value }) => (
+        <div key={label} className="eb-flex eb-items-center eb-justify-between">
+          <dt className="eb-text-muted-foreground">{label}</dt>
+          <dd>{value ?? '--'}</dd>
+        </div>
+      ))}
+    </dl>
+  );
+};
 
 const ReviewStep = () => {
   // const { getContentToken } = useContentData('steps.ReviewStep');
@@ -83,6 +104,28 @@ const ReviewStep = () => {
     }
   }, [activeStep]);
 
+  console.log(reviewData);
+
+  const individualDetailsArray = [];
+
+  for (const [, value] of Object.entries(reviewData?.individualDetails ?? {})) {
+    individualDetailsArray.push(value);
+  }
+  const individualDetails: IndividualDetails = (
+    individualDetailsArray?.[0] as { indDetails: IndividualDetails }
+  )?.indDetails;
+
+  const organizationDetailsArray = [];
+
+  for (const [, value] of Object.entries(
+    reviewData?.organizationDetails ?? {}
+  )) {
+    organizationDetailsArray.push(value);
+  }
+  const organizationDetails: OrganizationDetails = (
+    organizationDetailsArray?.[0] as { orgDetails: OrganizationDetails }
+  )?.orgDetails;
+
   return (
     <>
       <Stack className="eb-w-full">
@@ -93,99 +136,153 @@ const ReviewStep = () => {
             {reviewData?.status}
           </Title>
         </Group>
-
-        <Accordion type="multiple" className="eb-w-full">
-          <AccordionItem value="business-details">
-            <AccordionTrigger className="eb-uppercase">
-              Business Details
-            </AccordionTrigger>
-            <AccordionContent>
-              {reviewData?.organizationDetails && (
-                <CardReviewBusiness
-                  data={reviewData?.organizationDetails}
-                  title="Business Details"
-                  type="organization"
-                  onEdit={onEditBusiness}
+        <div className="eb-w-[28rem] eb-border eb-px-4">
+          <Accordion type="multiple" className="eb-w-full">
+            <AccordionItem value="individual-details">
+              <AccordionTrigger className="eb-uppercase">
+                Individual Details
+              </AccordionTrigger>
+              <AccordionContent>
+                <LabelValueTable
+                  data={[
+                    {
+                      label: 'First name',
+                      value: individualDetails?.firstName,
+                    },
+                    {
+                      label: 'Last name',
+                      value: individualDetails?.lastName,
+                    },
+                    {
+                      label: 'Date of birth',
+                      value: individualDetails?.birthDate,
+                    },
+                    {
+                      label: 'Phone',
+                      value: individualDetails?.phone?.phoneNumber,
+                    },
+                    {
+                      label: 'Job title',
+                      value: individualDetails?.jobTitle,
+                    },
+                    {
+                      label: 'Job title description',
+                      value: individualDetails?.jobTitleDescription,
+                    },
+                    {
+                      label: 'Nature of ownership',
+                      value: individualDetails?.natureOfOwnership,
+                    },
+                    {
+                      label: 'Sole owner?',
+                      value: individualDetails?.soleOwner ? 'Yes' : 'No',
+                    },
+                    {
+                      label: 'Country of Residence',
+                      value: individualDetails?.countryOfResidence,
+                    },
+                    {
+                      label: 'Address Line 1',
+                      value:
+                        individualDetails?.addresses?.[0]?.addressLines?.[0],
+                    },
+                    {
+                      label: 'Address Line 2',
+                      value:
+                        individualDetails?.addresses?.[0]?.addressLines?.[1],
+                    },
+                    {
+                      label: 'Address Line 3',
+                      value:
+                        individualDetails?.addresses?.[0]?.addressLines?.[3],
+                    },
+                    {
+                      label: 'City',
+                      value: individualDetails?.addresses?.[0]?.city,
+                    },
+                    {
+                      label: 'State',
+                      value: individualDetails?.addresses?.[0]?.state,
+                    },
+                    {
+                      label: 'Postal code',
+                      value: individualDetails?.addresses?.[0]?.postalCode,
+                    },
+                  ]}
                 />
-              )}
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="ownership">
-            <AccordionTrigger className="eb-uppercase">
-              Management & Ownership
-            </AccordionTrigger>
-            <AccordionContent>
-              <Stack className="eb-gap-4">
-                {reviewData?.individualDetails &&
-                  Object.keys(reviewData?.individualDetails).map((key: any) => {
-                    const indDetails = reviewData.individualDetails[key];
-
-                    return (
-                      <CardReviewIndividual
-                        data={indDetails}
-                        title="Individual & Managament"
-                        type="individual"
-                        onEdit={() => {
-                          onEditIndividual(true);
-                          setModalData(indDetails);
-                        }}
-                        key={indDetails?.id}
-                      />
-                    );
-                  })}
-              </Stack>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-
-        <Dialog open={edit} onOpenChange={onEditBusiness}>
-          {reviewData?.organizationDetails && (
-            <BusinessDetailsModal
-              formData={reviewData?.organizationDetails}
-              onCancel={(id: string) => {
-                onEditBusiness(false);
-                if (id) {
-                  refetch();
-                }
-              }}
-            />
-          )}
-        </Dialog>
-
-        <Dialog open={editIndividual} onOpenChange={onEditIndividual}>
-          {indData && (
-            <IndividualDetailsModal
-              formData={indData}
-              onCancel={(id: string) => {
-                onEditIndividual(false);
-                if (id) {
-                  refetch();
-                }
-              }}
-            />
-          )}
-        </Dialog>
-
-        <Dialog open={open} onOpenChange={setOpen}>
-          <Button
-            onClick={() => setOpen(true)}
-            type="button"
-            variant="outline"
-            className="eb-mt-4 eb-max-w-56"
-          >
-            <DialogTrigger>Click to add a decision maker</DialogTrigger>
-          </Button>
-          <IndividualDetailsModal
-            onCancel={(id: string) => {
-              setOpen(false);
-              if (id) {
-                refetch();
-              }
-            }}
-            parentPartyId={reviewData?.partyId}
-            create
-          />
-        </Dialog>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="organization-details">
+              <AccordionTrigger className="eb-uppercase">
+                Organization Details
+              </AccordionTrigger>
+              <AccordionContent>
+                <LabelValueTable
+                  data={[
+                    {
+                      label: 'Organization name',
+                      value: organizationDetails?.organizationName,
+                    },
+                    {
+                      label: 'Doing business as',
+                      value: organizationDetails?.dbaName,
+                    },
+                    {
+                      label: 'Organization type',
+                      value: organizationDetails?.organizationType,
+                    },
+                    {
+                      label: 'Organization description',
+                      value: organizationDetails?.organizationDescription,
+                    },
+                    {
+                      label: 'Website',
+                      value: organizationDetails?.website,
+                    },
+                    {
+                      label: 'Industry category',
+                      value: organizationDetails?.industryCategory,
+                    },
+                    {
+                      label: 'Industry type',
+                      value: organizationDetails?.industryType,
+                    },
+                    {
+                      label: 'Country of formation',
+                      value: organizationDetails?.countryOfFormation,
+                    },
+                    {
+                      label: 'Jurisdiction',
+                      value: organizationDetails?.jurisdiction,
+                    },
+                    {
+                      label: 'Business phone',
+                      value: organizationDetails?.phone?.phoneNumber,
+                    },
+                    {
+                      label: 'Entities in ownership?',
+                      value: organizationDetails?.entitiesInOwnership
+                        ? 'Yes'
+                        : 'No',
+                    },
+                    {
+                      label: 'Significant ownership?',
+                      value: organizationDetails?.significantOwnership
+                        ? 'Yes'
+                        : 'No',
+                    },
+                    {
+                      label: 'Trade over internet?',
+                      value: organizationDetails?.tradeOverInternet
+                        ? 'Yes'
+                        : 'No',
+                    },
+                  ]}
+                />
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
 
         <form noValidate onSubmit={form.handleSubmit(onSubmit)}>
           <NavigationButtons
