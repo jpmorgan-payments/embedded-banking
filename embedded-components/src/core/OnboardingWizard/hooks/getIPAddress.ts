78f9e75af5946c0c71318e34a2d0d59845e426b0
Andrey Aleshintsev <andrey.aleshintsev@jpmchase.com>
2024-08-19 10:07:49 -0400

    update to package and clean ip

--- embedded-components/src/core/OnboardingWizard/hooks/getIPAddress.ts
+++ embedded-components/src/core/OnboardingWizard/hooks/getIPAddress.ts
@@ -4,17 +4,17 @@ export const useIPAddress = () => {
   return useQuery({
     queryKey: ['useIPAddress'],
     queryFn: async () => {
-      if (process.env.NODE_ENV !== 'test') {
-        const { publicIpv4 } = await import('public-ip');
+      // if (process.env.NODE_ENV !== 'test') {
+      //   const { publicIpv4 } = await import('public-ip');
 
-        return publicIpv4({
-          fallbackUrls: [
-            `https://ifconfig.co/ip`,
-            'https://api64.ipify.org/',
-            'https://ipapi.co/ip',
-          ],
-        });
-      }
+      //   return publicIpv4({
+      //     fallbackUrls: [
+      //       `https://ifconfig.co/ip`,
+      //       'https://api64.ipify.org/',
+      //       'https://ipapi.co/ip',
+      //     ],
+      //   });
+      // }
 
       return new Promise((resolve) => {
         resolve('1.1.1.1');
