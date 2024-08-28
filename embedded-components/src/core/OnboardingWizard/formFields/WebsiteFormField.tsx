6d2df63d50688d2bba51fc2962afa19597d7b32d
Andrey Aleshintsev <andrey.aleshintsev@jpmchase.com>
2024-08-28 18:10:51 -0400

    update to latest

--- embedded-components/src/core/OnboardingWizard/formFields/WebsiteFormField.tsx
+++ embedded-components/src/core/OnboardingWizard/formFields/WebsiteFormField.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
+import { Check } from 'lucide-react';
+
 import {
   FormControl,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
+  Group,
   Input,
 } from '@/components/ui';
+
+import { CheckBoxFormFields } from './CheckboxFormField';
 
 const WebsiteFromField = ({
   name,
@@ -14,26 +20,41 @@ const WebsiteFromField = ({
   placeholderToken,
   required,
 }: any) => {
+  const [websiteAvailable, setWebsiteAvailable] = useState(false);
   return (
-    <FormField
-      control={form.control}
-      name={name}
-      render={({ field }) => (
-        <FormItem className="">
-          {labelToken && (
-            <FormLabel asterisk={required}>{labelToken}</FormLabel>
-          )}
-          <FormControl>
-            <Input
-              {...field}
-              required={required}
-              placeholder={placeholderToken}
-            />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
+    <Group className="eb-justify-start eb-align-baseline eb-grid-cols-3 eb-gap-16">
+      <CheckBoxFormFields
+        {...{
+          form,
+          onChange: setWebsiteAvailable,
+          name: 'websiteAvailable',
+          labelToken: 'Do you have a website?',
+          required,
+          className: 'eb-align-center',
+          classNameContainer: 'eb-flex eb-self-center',
+        }}
+      />
+      <FormField
+        control={form.control}
+        name={name}
+        render={({ field }) => (
+          <FormItem className="">
+            {labelToken && (
+              <FormLabel asterisk={required}>{labelToken}</FormLabel>
+            )}
+            <FormControl>
+              <Input
+                {...field}
+                required={required}
+                placeholder={placeholderToken}
+                disabled={!websiteAvailable}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </Group>
   );
 };
 
