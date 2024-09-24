ac62b5319921dc7d924d1236962139d548dbe63d
Hyungho Seo <hyungho.seo@jpmchase.com>
2024-09-24 16:28:46 -0400

    export TransactionsDisplay component

--- embedded-components/src/index.tsx
+++ embedded-components/src/index.tsx
@@ -7,3 +7,5 @@ export * from './core/EBComponentsProvider';
 
 export { StepperProvider } from './core/OnboardingWizard/Stepper/Stepper';
 export { initEBComponentsManager } from './vanilla/EBComponentsManager';
+
+export * from './core/TransactionsDisplay/TransactionsDisplay';
