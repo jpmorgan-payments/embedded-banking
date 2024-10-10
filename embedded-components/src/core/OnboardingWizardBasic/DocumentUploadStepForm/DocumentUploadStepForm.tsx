f0b29992142b0b0c83cb68f614ce398f2ea1ed49
Hyungho Seo <hyungho.seo@jpmchase.com>
2024-10-10 14:54:54 -0400

    Enhance DocumentUploadStep

--- embedded-components/src/core/OnboardingWizardBasic/DocumentUploadStepForm/DocumentUploadStepForm.tsx
+++ embedded-components/src/core/OnboardingWizardBasic/DocumentUploadStepForm/DocumentUploadStepForm.tsx
@@ -1,9 +1,12 @@
-import { useMemo } from 'react';
+import { Fragment, useEffect, useMemo } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
-import { DocumentTypeSmbdo } from '@/api/generated/smbdo.schemas';
+import {
+  useSmbdoGetClient,
+  useSmbdoListDocumentRequests,
+} from '@/api/generated/smbdo';
 import Dropzone from '@/components/ui/dropzone';
 import { useStepper } from '@/components/ui/stepper';
 import {
@@ -19,24 +22,80 @@ import {
 } from '@/components/ui';
 
 import { FormActions } from '../FormActions/FormActions';
+import { FormLoadingState } from '../FormLoadingState/FormLoadingState';
+import { useOnboardingContext } from '../OnboardingContextProvider/OnboardingContextProvider';
 import { DOCUMENT_TYPE_MAPPING } from '../utils/documentTypeMapping';
 
 export const DocumentUploadStepForm = () => {
   const { nextStep } = useStepper();
+  const { clientId } = useOnboardingContext();
 
-  const SAMPLE_DOCUMENTS: DocumentTypeSmbdo[] = ['DRIVERS_LICENSE', 'PASSPORT'];
+  // Fetch client data
+  const { data: clientData } = useSmbdoGetClient(clientId ?? '');
 
+  const DEMO = true;
+
+  // Fetch document requests
+  // eslint-disable-next-line prefer-const
+  let { data: documentRequestsData, status: documentRequestsStatus } =
+    useSmbdoListDocumentRequests({ clientId: clientId ?? '' });
+
+  useEffect(() => {
+    if (documentRequestsData && documentRequestsStatus === 'success') {
+      // clientData.outstanding.documentRequestIds
+    }
+  }, [documentRequestsData, documentRequestsStatus]);
+
+  if (DEMO) {
+    documentRequestsData = {
+      documentRequests: [
+        {
+          id: '50006',
+          country: 'US',
+          createdAt: '2022-11-18T12:28:11.232Z',
+          description: 'Please provide documents:\n - Signature Card',
+          requirements: [
+            {
+              documentTypes: ['SIGNATURE_CARD'],
+              level: 'PRIMARY',
+              minRequired: 1,
+            },
+          ],
+          outstanding: {
+            documentTypes: ['SIGNATURE_CARD'],
+          },
+          partyId: '2001133071',
+          status: 'ACTIVE',
+          updatedAt: '2022-11-18T12:28:11.232Z',
+          validForDays: 120,
+        },
+      ],
+      metadata: {
+        page: 0,
+        limit: 25,
+        total: 1,
+      },
+    };
+  }
+
+  // zod schema, dynamically generated based on the document types
   const DocumentUploadSchema = useMemo(() => {
     const schema: Record<string, z.ZodType<any>> = {};
-
-    SAMPLE_DOCUMENTS.forEach((documentType) => {
-      schema[documentType] = z
-        .array(z.instanceof(File))
-        .nonempty('Document is required');
+    documentRequestsData?.documentRequests.forEach((documentRequest) => {
+      if (!documentRequest.id) {
+        return;
+      }
+      const nestedSchema: Record<string, z.ZodType<any>> = {};
+      documentRequest.requirements?.forEach((requirement) => {
+        const documentType = requirement.documentTypes[0];
+        nestedSchema[documentType] = z
+          .array(z.instanceof(File))
+          .nonempty('Document is required');
+      });
+      schema[documentRequest.id] = z.object(nestedSchema);
     });
-
     return z.object(schema);
-  }, [SAMPLE_DOCUMENTS]);
+  }, [JSON.stringify(documentRequestsData)]);
 
   const form = useForm<z.infer<typeof DocumentUploadSchema>>({
     resolver: zodResolver(DocumentUploadSchema),
@@ -47,48 +106,102 @@ export const DocumentUploadStepForm = () => {
     nextStep();
   });
 
+  if (documentRequestsStatus === 'pending') {
+    return <FormLoadingState message="Fetching document requests..." />;
+  }
+
+  if (documentRequestsData?.documentRequests.length === 0 && !DEMO) {
+    return (
+      <Form {...form}>
+        <form
+          onSubmit={nextStep}
+          className="eb-grid eb-w-full eb-items-start eb-gap-6 eb-overflow-auto eb-p-1"
+        >
+          <Text>
+            No document requests found. Please proceed to the next step.
+          </Text>
+          <FormActions />
+        </form>
+      </Form>
+    );
+  }
+
   return (
     <Form {...form}>
       <form
         onSubmit={onSubmit}
         className="eb-grid eb-w-full eb-items-start eb-gap-6 eb-overflow-auto eb-p-1"
       >
-        <Text>You have been requested to provide the following documents.</Text>
-        <Separator />
-        {SAMPLE_DOCUMENTS.map((documentType) => (
-          <FormField
-            control={form.control}
-            name={documentType}
-            render={({ field: { onChange, ...fieldProps } }) => {
-              return (
-                <>
-                  <FormItem>
-                    <FormLabel asterisk>
-                      {DOCUMENT_TYPE_MAPPING[documentType].label}
-                    </FormLabel>
-                    <FormDescription>
-                      {DOCUMENT_TYPE_MAPPING[documentType].description}
-                    </FormDescription>
-                    <FormControl>
-                      <Dropzone
-                        containerClassName="eb-max-w-xl"
-                        {...fieldProps}
-                        multiple={false}
-                        accept={{
-                          'application/pdf': ['.pdf'],
-                        }}
-                        onChange={onChange}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
+        {documentRequestsData?.documentRequests.map((documentRequest) => {
+          const matchedParty = clientData?.parties?.find(
+            (party) => party.id === documentRequest.partyId
+          );
+          return (
+            <Fragment key={documentRequest.id}>
+              <div>
+                <Text className="eb-font-bold">
+                  Document request for:{' '}
+                  <span className="eb-font-normal eb-underline eb-underline-offset-2">
+                    {matchedParty?.organizationDetails?.organizationName ??
+                      [
+                        matchedParty?.individualDetails?.firstName,
+                        matchedParty?.individualDetails?.middleName,
+                        matchedParty?.individualDetails?.lastName,
+                      ].join(' ') ??
+                      'N/A'}
+                  </span>{' '}
+                  <span className="eb-text-xs eb-font-normal eb-lowercase eb-text-muted-foreground">
+                    ({matchedParty?.roles?.join(', ')})
+                  </span>
+                </Text>
+                {documentRequest.description?.split('\n').map((item, key) => (
+                  <Text key={key} className="eb-font-semibold">
+                    {item}
+                  </Text>
+                ))}
+              </div>
+              <Separator />
+              {documentRequest.requirements?.map((requirement, index) => {
+                const documentType = requirement.documentTypes[0];
+                return (
+                  <FormField
+                    key={index}
+                    control={form.control}
+                    name={`${documentRequest.id}.${documentType}`}
+                    render={({ field: { onChange, ...fieldProps } }) => {
+                      return (
+                        <>
+                          <FormItem>
+                            <FormLabel asterisk>
+                              {DOCUMENT_TYPE_MAPPING[documentType].label}
+                            </FormLabel>
+                            <FormDescription>
+                              {DOCUMENT_TYPE_MAPPING[documentType].description}
+                            </FormDescription>
+                            <FormControl>
+                              <Dropzone
+                                containerClassName="eb-max-w-xl"
+                                {...fieldProps}
+                                multiple={(requirement.minRequired ?? 0) > 1}
+                                accept={{
+                                  'application/pdf': ['.pdf'],
+                                }}
+                                onChange={onChange}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
 
-                  <Separator />
-                </>
-              );
-            }}
-          />
-        ))}
+                          <Separator />
+                        </>
+                      );
+                    }}
+                  />
+                );
+              })}
+            </Fragment>
+          );
+        })}
 
         <FormActions />
       </form>
